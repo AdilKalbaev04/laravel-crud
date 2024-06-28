64bf3ddac3db9765223720afe3599e922964095a
AdilKalbaev04 <adilkalbaev2004@gmail.com>
2024-06-28 10:08:48 +0600

    xz 2

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -2,9 +2,41 @@
 
 @section('title', 'Корзина')
 
+@push('styles')
+    <style>
+        .cart-container {
+            margin-top: 50px;
+        }
+        .cart-title {
+            font-size: 2rem;
+            margin-bottom: 20px;
+            text-align: center;
+        }
+        .cart-table {
+            margin-bottom: 30px;
+        }
+        .cart-table th, .cart-table td {
+            vertical-align: middle;
+            text-align: center;
+        }
+        .cart-total {
+            font-size: 1.5rem;
+            text-align: right;
+        }
+        .cart-actions {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        .cart-actions .btn {
+            min-width: 150px;
+        }
+    </style>
+@endpush
+
 @section('content')
-    <div class="container mt-5">
-        <h2>Корзина</h2>
+    <div class="container cart-container">
+        <h2 class="cart-title">Корзина</h2>
         @if(session('success'))
             <div class="alert alert-success">
                 {{ session('success') }}
@@ -12,7 +44,7 @@
         @endif
         <div class="row">
             <div class="col-12">
-                <table class="table">
+                <table class="table table-hover cart-table">
                     <thead>
                         <tr>
                             <th>Название</th>
@@ -43,18 +75,20 @@
                     </tbody>
                 </table>
                 @if($total > 0)
-                    <div class="text-end">
+                    <div class="cart-total">
                         <strong>Общая стоимость: {{ $total }} руб.</strong>
                     </div>
                 @endif
+                <div class="cart-actions mt-4">
+                    <a href="{{ route('products.index') }}" class="btn btn-secondary">Продолжить покупки</a>
+                    @if($total > 0)
+                        <a href="#" class="btn btn-primary">Оформить заказ</a>
+                    @endif
+                </div>
             </div>
         </div>
     </div>
 @endsection
-
-@push('styles')
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-@endpush
 
 @push('scripts')
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76A8fCgtSIsQltt+KY00Nv3G5r7bLeYem5vC3N0cpM5j7OcsgF1wtAby6pGFM90" crossorigin="anonymous"></script>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title', 'Магазин')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/font-awesome@5.15.4/css/all.min.css" rel="stylesheet">
-    <title>@yield('title', 'Магазин')</title>
     <style>
         .swiper-container {
             width: 100%;
@@ -17,25 +17,16 @@
             height: 100%;
             object-fit: cover;
         }
-
-        .swiper-button-next,
-        .swiper-button-prev {
-            top: 35%;
-            transform: translateY(-45%);
-        }
-
         .rating {
             unicode-bidi: bidi-override;
             direction: rtl;
             text-align: center;
         }
-
         .rating > span {
             display: inline-block;
             position: relative;
             width: 1.1em;
         }
-
         .rating > span:hover:before,
         .rating > span:hover ~ span:before {
             content: "\2605";
@@ -49,28 +40,32 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-warning">
         <div class="container-fluid">
             <a class="navbar-brand h1" href="{{ route('products.index') }}">Магазин</a>
-            <div class="collapse navbar-collapse">
-                <ul class="navbar-nav ml-auto">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="btn btn-primary" href="{{ route('products.create') }}">Добавить товар</a>
+                        <a class="nav-link" href="{{ route('products.create') }}">Добавить товар</a>
                     </li>
                     <li class="nav-item">
-                        <a class="btn btn-success" href="{{ route('cart.index') }}">Корзина</a>
+                        <a class="nav-link" href="{{ route('cart.index') }}">Корзина</a>
                     </li>
                     <li class="nav-item">
-                        <a class="btn btn-success" href="{{ url('/about') }}">О нас</a>
+                        <a class="nav-link" href="{{ url('/about') }}">О нас</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/register') }}">Регистрация</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/login') }}">Авторизация</a>
                     </li>
                     <li class="nav-item">
                         <form action="{{ route('logout') }}" method="POST">
                             @csrf
-                            <button type="submit" class="btn btn-danger">Выйти</button>
+                            <button type="submit" class="btn">Выйти</button>
                         </form>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-secondary" href="{{ url('/register') }}">Регистрация</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="btn btn-secondary" href="{{ url('/login') }}">Авторизация</a>
                     </li>
                 </ul>
             </div>
@@ -78,6 +73,14 @@
     </nav>
 
     @yield('content')
+
+    <footer class="bg-light text-center text-lg-start mt-5">
+        <div class="container p-4">
+            <div class="text-center p-3">
+                &copy; 2024 Магазин. Все права защищены.
+            </div>
+        </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76A8fCgtSIsQltt+KY00Nv3G5r7bLeYem5vC3N0cpM5j7OcsgF1wtAby6pGFM90" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
